d0d43d5cfb89ce9478d826b06c9e720aa5927b10
staniamyroh <tsaniamyroh@gmail.com>
2022-10-15 18:02:00 +0700

    tugas 5

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Stani Amyroh
+            Saya tinggal di daerah Tanjung Hulu
+            olahraga yang saya sukai adalah 
+            Senam dan Joging";
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php   echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Stani </li>
+        <li>Nama Belakang : Amyroh</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Yam Sabran , Kelurahan Tanjung Hulu</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Gorontalo</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Senam</li>
+        <li>Joging</li>
+    </ul>
+</section>
